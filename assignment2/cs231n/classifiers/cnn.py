2383f05d1e0d358f11efc3270639e3af8ca54f46
Michal Kosinski <kosinski.michal@gmail.com>
2018-01-07 17:19:31 +0100

    implement ThreeLayersConvnet

--- assignment2/cs231n/classifiers/cnn.py
+++ assignment2/cs231n/classifiers/cnn.py
@@ -48,7 +48,22 @@ class ThreeLayerConvNet(object):
         # hidden affine layer, and keys 'W3' and 'b3' for the weights and biases   #
         # of the output affine layer.                                              #
         ############################################################################
-        pass
+
+        input_filters, Hin, Win = input_dim
+        hidden_input_size = np.int(num_filters * Hin / 2 * Win / 2) # accounting for maxpool reduction by 2
+
+        # conv layer
+        self.params['W1'] = np.random.randn(num_filters, input_filters, filter_size, filter_size) * weight_scale
+        self.params['b1'] = np.zeros(num_filters,)
+
+        # hidden affine layer
+        self.params['W2'] = np.random.randn(hidden_input_size, hidden_dim) * weight_scale
+        self.params['b2'] = np.zeros(hidden_dim,)
+
+        # output affine layer
+        self.params['W3'] = np.random.randn(hidden_dim, num_classes) * weight_scale
+        self.params['b3'] = np.zeros(num_classes,)
+
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -66,7 +81,6 @@ class ThreeLayerConvNet(object):
         W1, b1 = self.params['W1'], self.params['b1']
         W2, b2 = self.params['W2'], self.params['b2']
         W3, b3 = self.params['W3'], self.params['b3']
-
         # pass conv_param to the forward pass for the convolutional layer
         filter_size = W1.shape[2]
         conv_param = {'stride': 1, 'pad': (filter_size - 1) // 2}
@@ -77,10 +91,18 @@ class ThreeLayerConvNet(object):
         scores = None
         ############################################################################
         # TODO: Implement the forward pass for the three-layer convolutional net,  #
-        # computing the class scores for X and storing them in the scores          #
+        # computing the class s)ores for X and storing them in the scores          #
         # variable.                                                                #
         ############################################################################
-        pass
+        #conv - relu - 2x2 max pool - affine - relu - affine - softmax
+
+        conv_out, conv_cache = conv_forward_fast(X, W1, b1, conv_param)
+        relu1_out, relu1_cache = relu_forward(conv_out)
+        pool_out, pool_cache = max_pool_forward_fast(relu1_out, pool_param)
+        hidden_out, hidden_cache = affine_forward(pool_out, W2, b2)
+        relu2_out, relu2_cache = relu_forward(hidden_out)
+        scores, scores_cache = affine_forward(relu2_out, W3, b3)
+
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -95,7 +117,20 @@ class ThreeLayerConvNet(object):
         # data loss using softmax, and make sure that grads[k] holds the gradients #
         # for self.params[k]. Don't forget to add L2 regularization!               #
         ############################################################################
-        pass
+        loss, loss_grad = softmax_loss(scores, y)
+        scores_grad, dW3, db3 = affine_backward(loss_grad, scores_cache)
+        relu2_grad = relu_backward(scores_grad, relu2_cache)
+        hidden_grad, dW2, db2 = affine_backward(relu2_grad, hidden_cache)
+        pool_grad = max_pool_backward_fast(hidden_grad, pool_cache)
+        relu1_grad = relu_backward(pool_grad, relu1_cache)
+        _, dW1, db1 = conv_backward_fast(relu1_grad, conv_cache)
+
+        grads['W3'] = dW3
+        grads['b3'] = db3
+        grads['W2'] = dW2
+        grads['b2'] = db2
+        grads['W1'] = dW1
+        grads['b1'] = db1
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
